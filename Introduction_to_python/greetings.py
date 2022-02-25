b65ebed86ce25ab5d2839002267669133b3c3ed4
leo <40744320+leogithubid@users.noreply.github.com>
2022-02-25 13:06:40 +1100

    moved folders around to course1

---
+++ Introduction_to_python/greetings.py
@@ -0,0 +1,13 @@
+"""This program will ask for user name
+and greet the user """
+name = input('Please enter your name:')
+print('Hello ' + name)
+
+x = 5
+y = "John"
+z = "UIO"
+try:
+    print(x + x + z)
+except:
+    print("type error")
+
